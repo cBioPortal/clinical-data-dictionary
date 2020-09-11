8b4e7f70c58c73046ae772e8913fce8afb5e45e7
Robert Sheridan <sheridan@cbio.mskcc.org>
2020-09-11 11:18:29 -0400

    adjust namespace/graph name for TopBraid 6.3
    also parameterize namespace/graph properties for sparql

--- src/main/java/org/cbioportal/cdd/repository/topbraid/ClinicalAttributeMetadataRepositoryTopBraidImpl.java
+++ src/main/java/org/cbioportal/cdd/repository/topbraid/ClinicalAttributeMetadataRepositoryTopBraidImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2019 Memorial Sloan-Kettering Cancer Center.
+ * Copyright (c) 2018-2020 Memorial Sloan-Kettering Cancer Center.
  *
  * This library is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
@@ -27,6 +27,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Repository;
 
@@ -39,48 +40,68 @@ public class ClinicalAttributeMetadataRepositoryTopBraidImpl extends TopBraidRep
 
     private final static Logger logger = LoggerFactory.getLogger(ClinicalAttributeMetadataRepositoryTopBraidImpl.class);
 
-    private final static String GET_OVERRIDES_SPARQL_QUERY_STRING = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
-        "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
-        "PREFIX cdd:<http://data.mskcc.org/ontologies/clinical_data_dictionary/> " +
-        "PREFIX skos:<http://www.w3.org/2004/02/skos/core#> " +
-        "SELECT DISTINCT ?study_id ?column_header (SAMPLE(?PriorityValue) AS ?priority) (SAMPLE(?AttributeTypeValue) AS ?attribute_type) (SAMPLE(?DatatypeValue) AS ?datatype) (SAMPLE(?DescriptionValue) AS ?description) (SAMPLE(?DisplayNameValue) AS ?display_name) " +
-        "WHERE { " +
-        "    GRAPH <urn:x-evn-master:clinical_data_dictionary> { " +
-        "        ?node rdf:type ?type. " +
-        "        ?node skos:broader ?parent. " +
-        "        ?parent cdd:StudyId ?study_id. " +
-        "        ?parent skos:broader ?grandparent. " +
-        "        ?grandparent skos:prefLabel ?column_header. " +
-        "        OPTIONAL{?node cdd:PriorityValue ?PriorityValue}. " +
-        "        OPTIONAL{?node cdd:AttributeTypeValue ?AttributeTypeValue}. " +
-        "        OPTIONAL{?node cdd:DatatypeValue ?DatatypeValue}. " +
-        "        OPTIONAL{?node cdd:DescriptionValue ?DescriptionValue}. " +
-        "        OPTIONAL{?node cdd:DisplayNameValue ?DisplayNameValue}. " +
-        "    } " +
-        "} " +
-        "GROUP BY ?study_id ?column_header " +
-        "ORDER BY ?study_id ?column_header " +
-        "VALUES ?type {cdd:ClinicalAttributeOverridePriorityValue cdd:ClinicalAttributeOverrideAttributeTypeValue cdd:ClinicalAttributeOverrideDatatypeValue cdd:ClinicalAttributeOverrideDescriptionValue cdd:ClinicalAttributeOverrideDisplayNameValue}";
 
-    private final static String GET_CLINICAL_ATTRIBUTES_SPARQL_QUERY_STRING = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
-        "PREFIX cdd:<http://data.mskcc.org/ontologies/clinical_data_dictionary/> " +
-        "PREFIX skos:<http://www.w3.org/2004/02/skos/core#> " +
-        "SELECT ?column_header ?display_name ?attribute_type ?datatype ?description ?priority " +
-        "WHERE { " +
-        "    GRAPH <urn:x-evn-master:clinical_data_dictionary> { " +
-        "        ?subject skos:prefLabel ?column_header. " +
-        "        ?subject cdd:AttributeType ?attribute_type. " +
-        "        ?subject cdd:Datatype ?datatype. " +
-        "        ?subject cdd:Description ?description. " +
-        "        ?subject cdd:DisplayName ?display_name. " +
-        "        ?subject cdd:Priority ?priority. " +
-        "    } " +
-        "}";
+    @Value("${topbraid.cdd_namespace_prefix:http://data.mskcc.org/ontologies/ClinicalDataDictionary#}")
+    private String topBraidCddNamespacePrefix;
+
+    @Value("${topbraid.cdd_graph_id:urn:x-evn-master:cdd}")
+    private String topBraidCddGraphId;
+
+    private String overridesQuery = null;
+    private String attributesQuery = null;
+
+    private String getOverridesQuery() {
+        if (overridesQuery == null) {
+            overridesQuery = 
+                    "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
+                    "PREFIX cdd:<" + topBraidCddNamespacePrefix + "> " +
+                    "PREFIX skos:<http://www.w3.org/2004/02/skos/core#> " +
+                    "SELECT DISTINCT ?study_id ?column_header (SAMPLE(?PriorityValue) AS ?priority) (SAMPLE(?AttributeTypeValue) AS ?attribute_type) (SAMPLE(?DatatypeValue) AS ?datatype) (SAMPLE(?DescriptionValue) AS ?description) (SAMPLE(?DisplayNameValue) AS ?display_name) " +
+                    "WHERE { " +
+                    "    GRAPH <" + topBraidCddGraphId + "> { " +
+                    "        ?node rdf:type ?type. " +
+                    "        ?node skos:broader ?parent. " +
+                    "        ?parent cdd:StudyId ?study_id. " +
+                    "        ?parent skos:broader ?grandparent. " +
+                    "        ?grandparent skos:prefLabel ?column_header. " +
+                    "        OPTIONAL{?node cdd:PriorityValue ?PriorityValue}. " +
+                    "        OPTIONAL{?node cdd:AttributeTypeValue ?AttributeTypeValue}. " +
+                    "        OPTIONAL{?node cdd:DatatypeValue ?DatatypeValue}. " +
+                    "        OPTIONAL{?node cdd:DescriptionValue ?DescriptionValue}. " +
+                    "        OPTIONAL{?node cdd:DisplayNameValue ?DisplayNameValue}. " +
+                    "    } " +
+                    "} " +
+                    "GROUP BY ?study_id ?column_header " +
+                    "ORDER BY ?study_id ?column_header " +
+                    "VALUES ?type {cdd:ClinicalAttributeOverridePriorityValue cdd:ClinicalAttributeOverrideAttributeTypeValue cdd:ClinicalAttributeOverrideDatatypeValue cdd:ClinicalAttributeOverrideDescriptionValue cdd:ClinicalAttributeOverrideDisplayNameValue}";
+        }
+        return overridesQuery;
+    }
+
+    private String getAttributesQuery() {
+        if (attributesQuery == null) {
+            attributesQuery =
+                    "PREFIX cdd:<" + topBraidCddNamespacePrefix + "> " +
+                    "PREFIX skos:<http://www.w3.org/2004/02/skos/core#> " +
+                    "SELECT ?column_header ?display_name ?attribute_type ?datatype ?description ?priority " +
+                    "WHERE { " +
+                    "    GRAPH <" + topBraidCddGraphId + "> { " +
+                    "        ?subject skos:prefLabel ?column_header. " +
+                    "        ?subject cdd:AttributeType ?attribute_type. " +
+                    "        ?subject cdd:Datatype ?datatype. " +
+                    "        ?subject cdd:Description ?description. " +
+                    "        ?subject cdd:DisplayName ?display_name. " +
+                    "        ?subject cdd:Priority ?priority. " +
+                    "    } " +
+                    "}";
+        }
+        return attributesQuery;
+    }
 
     public ArrayList<ClinicalAttributeMetadata> getClinicalAttributeMetadata() {
         logger.info("Fetching clinical attribute metadata from TopBraid...");
         try {
-            ArrayList<ClinicalAttributeMetadata> list = new ArrayList<ClinicalAttributeMetadata>(super.query(GET_CLINICAL_ATTRIBUTES_SPARQL_QUERY_STRING, new ParameterizedTypeReference<List<ClinicalAttributeMetadata>>(){}));
+            ArrayList<ClinicalAttributeMetadata> list = new ArrayList<ClinicalAttributeMetadata>(super.query(getAttributesQuery(), new ParameterizedTypeReference<List<ClinicalAttributeMetadata>>(){}));
             return list;
         } catch (TopBraidException e) {
             logger.error("Problem connecting to TopBraid");
@@ -91,7 +112,7 @@ public class ClinicalAttributeMetadataRepositoryTopBraidImpl extends TopBraidRep
     public Map<String, ArrayList<ClinicalAttributeMetadata>> getClinicalAttributeMetadataOverrides() {
         logger.info("Fetch clinical attribute metadata overrides from TopBraid...");
         try {
-            List<ClinicalAttributeMetadata> overridesList = super.query(GET_OVERRIDES_SPARQL_QUERY_STRING, new ParameterizedTypeReference<List<ClinicalAttributeMetadata>>(){});
+            List<ClinicalAttributeMetadata> overridesList = super.query(getOverridesQuery(), new ParameterizedTypeReference<List<ClinicalAttributeMetadata>>(){});
             Map<String, ArrayList<ClinicalAttributeMetadata>> overridesStudyMap = new HashMap<>();
             for (ClinicalAttributeMetadata clinicalAttributeMetadata : overridesList) {
                 if (!overridesStudyMap.containsKey(clinicalAttributeMetadata.getStudyId())) {
